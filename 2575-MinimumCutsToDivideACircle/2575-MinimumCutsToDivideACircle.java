6570c6ebd5ab687d49646101fdd305be2373d496
THARANI DHARAN R <dharanidharan0404@gmail.com>
2026-07-09 09:47:04 +0530

    Create 2575-MinimumCutsToDivideACircle

---
+++ 2575-MinimumCutsToDivideACircle/2575-MinimumCutsToDivideACircle.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 9:47:04 AM
+class Solution {
+    public int numberOfCuts(int n) {
+      //  int s=0;
+        if(n%2==0 || n==1)
+        {
+            n/=2;
+        }
+        
+        return n;
+    }
+}
